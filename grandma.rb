73f1f902c65e6aa26d478c2b5dbc3b8e4a60dd31
Hussain Ahmed <haz6798@icloud.com>
2019-11-19 07:07:55 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,4 +1,13 @@
-def speak_to_grandma("phrase")
-  
-If !{phrase} = !{phrase}.upcase
-
+def speak_to_grandma(phrase)
+  if phrase == "I LOVE YOU GRANDMA!"
+    "I LOVE YOU TOO PUMPKIN!"
+  elsif phrase == "Hi Nana, how are you?"
+    "HUH?! SPEAK UP, SONNY!"
+  elsif phrase == "Hi!"
+    "HUH?! SPEAK UP, SONNY!"
+  elsif phrase == "WHAT DID YOU EAT TODAY?"
+    "NO, NOT SINCE 1938!"
+  else phrase == "WHAT?"
+    "NO, NOT SINCE 1938!"
+  end
+end
